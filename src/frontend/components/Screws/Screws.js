9b7aa3c0286e33f6c171daeb0481c42d29a85c4d
William Velazquez <william.velazquez.a@gmail.com>
2020-01-15 17:02:29 -0600

    Creating Screws and Vents Components

---
+++ src/frontend/components/Screws/Screws.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import './styles.css';
+
+const Screws = (props) => {
+  const {quantity} = props;
+
+  const createScrews = () => {
+    let screws = [];
+    for (let index = 0; index < quantity; index++) {
+      screws.push(
+        <div className={`circle__screw`}>
+          <div className={`circle__screw--border`}>
+            <div className={`circle__screw--cover ${props.color||'gray'}`}>
+              <div className='circle__screw--detail'>
+                <div className="circle__screw--first-detail"></div>
+                <div className="circle__screw--second-detail"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+    return screws;
+  }
+
+  return (
+    <div className='screws__container'>
+      {
+        createScrews()
+      }
+    </div>
+  );
+};
+
+export default Screws;
---
+++ src/frontend/components/Vents/Vents.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './styles.css';
+
+const Vents = (props) => {
+  const {quantity} = props;
+
+  const createVents = () => {
+    let vents = [];
+    for (let index = 0; index < quantity; index++) {
+      vents.push(<div className='vents__item' key={index} ></div>);
+    }
+    return vents;
+  }
+
+  return (
+    <div className='vents__container'>
+      {
+        createVents()
+      }
+    </div>
+  );
+};
+
+export default Vents;
