988daf0b070ca55121dc28272b76db907e3acde1
William Velazquez <william.velazquez.a@gmail.com>
2020-01-15 03:31:32 -0600

    Implementing Screen Component

--- src/frontend/containers/Pokedex.js
+++ src/frontend/containers/Pokedex.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import MainLayout from '../components/MainLayout/MainLayout';
+import Screen from '../components/Screen/Screen';
 import EllipsisLoader from '../components/EllipsisLoader/EllipsisLoader';
 
 const Pokedex = () => {
   return (
     <MainLayout>
-      <div style={{ display: 'flex', justifyContent: 'center' }}><EllipsisLoader /></div>
+      <Screen>
+        <div style={{ display: 'flex', justifyContent: 'center' }}><EllipsisLoader /></div>
+      </Screen>
     </MainLayout>
   );
 };
