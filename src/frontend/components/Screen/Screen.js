8a143db4ca75667abaf492ba49a465b02fee4cf8
William Velazquez <william.velazquez.a@gmail.com>
2020-01-15 17:06:49 -0600

    Implementing Screen Plate Details (Adding Screws, Led and Vents)

--- src/frontend/components/Screen/Screen.js
+++ src/frontend/components/Screen/Screen.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import Screws from '../Screws/Screws';
+import CircleLed from '../CircleLed/CircleLed';
+import Vents from '../Vents/Vents';
 import './styles.css';
 
 const Screen = (props) => {
@@ -9,7 +12,14 @@ const Screen = (props) => {
 			<div className='screen__plate'>
 				<div className='screen__glass'>
 					{children}
-				</div>
+        </div>
+        <div className='screen__plate__top--details'>
+          <Screws quantity={2} />
+        </div>
+        <div className='screen__plate__bottom--details'>
+          <CircleLed color='red' size='small' fixed />
+          <Vents quantity={3} />
+        </div>
 			</div>
     </section>
   );
