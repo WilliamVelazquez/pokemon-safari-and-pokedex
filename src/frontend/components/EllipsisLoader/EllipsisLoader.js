beaf910e708cdb4ba294e0433796bd17927dca0c
William Velazquez <william.velazquez.a@gmail.com>
2020-01-14 20:47:04 -0600

    Creating EllipsisLoader and Wave Components

---
+++ src/frontend/components/EllipsisLoader/EllipsisLoader.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './styles.css';
+
+const EllipsisLoader = () => (
+  <div className='ellipsis'>
+    <div>
+      <div />
+    </div>
+    <div>
+      <div />
+    </div>
+    <div>
+      <div />
+    </div>
+    <div>
+      <div />
+    </div>
+    <div>
+      <div />
+    </div>
+  </div>
+);
+
+export default EllipsisLoader;
