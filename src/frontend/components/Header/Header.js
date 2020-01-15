8de1b62a8b27cc85b32eb4ffc1d36ad0dc5c5d70
William Velazquez <william.velazquez.a@gmail.com>
2020-01-15 02:58:31 -0600

    Updating Background Colors and Adding CircleLeds on the Header

--- src/frontend/components/Header/Header.js
+++ src/frontend/components/Header/Header.js
@@ -1,28 +1,38 @@
 import React from 'react';
 
+import CircleLed from '../CircleLed/CircleLed';
+
 import PokemonLogo from '../../assets/images/logo.png';
 import PokeballNav from '../../assets/images/pokeball-nav.png';
 
 import './styles.css';
 
 const Header = () =>(
-  <header className='header'>
-    <div className='header__logo_container'>
-      <img className='header__img' src={PokemonLogo} alt='Pokémon Logo' />
-      <span className='header__title'>Pokédex</span>
-    </div>
-    <div className='header__menu'>
-      <div className='header__menu--profile'>
-        <img src={PokeballNav} alt='Nav Icon' />
-        <p>Menú</p>
+  <>
+    <header className='header'>
+      <div className='header__logo_container'>
+        {/* <img className='header__img' src={PokemonLogo} alt='Pokémon Logo' />
+        <span className='header__title'>Pokédex</span> */}
       </div>
-      <ul>
-        <li><a href='/'>Mi PC</a></li>
-        <li><a href='/'>Safari</a></li>
-        <li><a href='/'>Pokédex</a></li>
-      </ul>
+      <div className='header__menu'>
+        <div className='header__menu--profile'>
+          <img src={PokeballNav} alt='Nav Icon' />
+          <p>Menú</p>
+        </div>
+        <ul>
+          <li><a href='/'>Mi PC</a></li>
+          <li><a href='/'>Safari</a></li>
+          <li><a href='/'>Pokédex</a></li>
+        </ul>
+      </div>
+    </header>
+    <CircleLed color='blue' size='big' main />
+    <div className='header__leds'>
+      <CircleLed color='red' size='small' />
+      <CircleLed color='yellow' size='small' />
+      <CircleLed color='green' size='small' />
     </div>
-  </header>
+  </>
 );
 
 export default Header;
