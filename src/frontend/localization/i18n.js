7a908e5cf9ed123b621bbb0b06dcf5ac53f2979e
William Velazquez <william.velazquez.a@gmail.com>
2020-01-15 20:43:48 -0600

    Adding Localization Dependencies and Creating Localization Settings and Translations Files

---
+++ src/frontend/localization/i18n.js
@@ -0,0 +1,21 @@
+import i18n from "i18next";
+import Backend from "i18next-xhr-backend";
+import { resources } from "./translations";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+
+i18n
+  .use(Backend)// load translation using xhr -> see /public/locales
+  .use(LanguageDetector)// detect user language
+  .use(initReactI18next)// pass the i18n instance to react-i18next
+  .init({
+    resources,
+    lng: "en",
+    fallbackLng: "en",
+    debug: true,
+    interpolation: {
+      escapeValue: false,// not needed for react as it escapes by default
+    },
+  });
+
+export default i18n;
---
+++ src/frontend/localization/translations.js
@@ -0,0 +1,18 @@
+const resources = {
+  en: {
+    translation: {
+      "menu": {
+        my_pc: "My PC"
+      }
+    },
+  },
+  es: {
+    translation: {
+      "menu": {
+        my_pc: "Mi PC"
+      }
+    },
+  },
+};
+
+export { resources };
