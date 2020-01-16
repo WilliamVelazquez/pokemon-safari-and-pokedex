5e5156374ce54b7c2080ed58ca476694b27629c0
William Velazquez <william.velazquez.a@gmail.com>
2020-01-16 13:30:44 -0600

    Creating Store, Setting Redux Provider and Creating reducers File

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,10 +1,24 @@
 import React from "react";
 import { render } from "react-dom";
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import "babel-polyfill";
 import './localization/i18n';
 
+import reducer from './reducers';
 import App from "./routes/App";
 
+const initialState = {
+  // 'language': localStorage.getItem('language') || 'en',
+  'pokemons': [],
+  'myPokemons': [],
+};
+const store = createStore(reducer, initialState);
 const $AppContainer = document.getElementById("app");
 
-render(<App />, $AppContainer);
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  $AppContainer
+);
