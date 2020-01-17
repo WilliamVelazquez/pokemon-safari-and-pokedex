caf0b39e33241d50c20f814489b7232af571bce2
William Velazquez <william.velazquez.a@gmail.com>
2020-01-17 12:27:23 -0600

    Adding SSR Dependencies and Creating Simple Server with Express and DotEnv Settings

---
+++ src/server/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const ENV = process.env.NODE_ENV;
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.get('*', (request, response) => {
+  response.send({ test: true });
+});
+
+app.listen(PORT, (error) => {
+  if (error) console.log(error);
+  console.log(`Server running on PORT: ${PORT}`);
+})
