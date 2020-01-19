18ad32d2215eedaa2606067f275a4f897af9d130
William Velazquez <william.velazquez.a@gmail.com>
2020-01-19 02:40:12 -0600

    Adding Type Colors Constant and Creating PokemonInfoCard Component

---
+++ src/frontend/components/PokemonInfoCard/PokemonInfoCard.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { isEmptyObject } from 'Utils/functions';
+import { TYPES_COLORS } from 'Constants/app';
+
+import PokeballNav from '../../assets/images/pokeball-nav.png';
+
+import './styles.css';
+
+const PokemonInfoCard = (props) => {
+	const { pokemon, handleClose } = props;
+	console.log(pokemon);
+  return (
+    <div className='infoCard__container animation'>
+			<div className='infoCard__sprite'>
+				{ 
+					isEmptyObject(pokemon.baseData.sprites.front_default) ?
+					<img className='rotating' src={PokeballNav} alt={`${pokemon.name} image`} />
+					:
+					<img src={pokemon.baseData.sprites.front_default} alt={`${pokemon.name} image`} />
+				}
+			</div>
+			<div className='infoCard__data'>
+				<div className='infoCard__data__title'>
+					<h2>{pokemon.name}</h2>
+					<h3>#{pokemon.id}</h3>
+				</div>
+				<div className='infoCard__data__types'>
+					{
+						pokemon.baseData.types.map((type) => (
+							<span key={type.slot} className='infoCard__data__types--name' style={{backgroundColor: `#${TYPES_COLORS[type.type.name]}`}}>
+								{type.type.name}
+							</span>
+						))
+					}
+				</div>
+				<div className='infoCard__data__description'>
+					{
+						<span>
+							{ 
+								pokemon.flavor_text_entries.find((flavor) => 
+									flavor.language.name === (localStorage.getItem('language') || 'en')
+								).flavor_text
+							}
+						</span>
+					}
+				</div>
+			</div>
+			<div className='infoCard__close-btn' onClick={handleClose}>x</div>
+		</div>
+  );
+};
+
+export default PokemonInfoCard;
--- src/frontend/constants/app.js
+++ src/frontend/constants/app.js
@@ -2,3 +2,24 @@ export const API_BASE_URL = 'http://localhost:3002/api/pokemons/';
 export const INITIAL_POKEMON_NUMBER = 1;
 export const LAST_POKEMON_NUMBER = 151;
 export const POKEMON_QUANTITY_TO_LOAD = 10;
+
+export const TYPES_COLORS = {
+	bug: 'B1C12E',
+	dark: '4F3A2D',
+	dragon: '755EDF',
+	electric: 'FCBC17',
+	fairy: 'F4B1F4',
+	fighting: '823551D',
+	fire: 'E73B0C',
+	flying: 'A3B3F7',
+	ghost: '6060B2',
+	grass: '74C236',
+	ground: 'D3B357',
+	ice: 'A3E7FD',
+	normal: 'C8C4BC',
+	poison: '934594',
+	psychic: 'ED4882',
+	rock: 'B9A156',
+	steel: 'B5B5C3',
+	water: '3295F6',
+};
