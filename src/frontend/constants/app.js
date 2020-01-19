6a33cfb21744ed2cf6a4ef1d567beb6c6a6592bf
William Velazquez <william.velazquez.a@gmail.com>
2020-01-18 22:44:59 -0600

    Adding Constants for the Pokedex and Updating InitialState

--- src/frontend/constants/app.js
+++ src/frontend/constants/app.js
@@ -1,1 +1,4 @@
 export const API_BASE_URL = 'http://localhost:3002/api/pokemons/';
+export const INITIAL_POKEMON_NUMBER = 1;
+export const LAST_POKEMON_NUMBER = 151;
+export const POKEMON_QUANTITY_TO_LOAD = 10;
--- src/frontend/index.js
+++ src/frontend/index.js
@@ -5,6 +5,8 @@ import { createStore, compose } from 'redux';
 import 'babel-polyfill';
 import './localization/i18n';
 
+import { INITIAL_POKEMON_NUMBER } from 'Constants/app';
+
 import reducer from './reducers';
 import App from './routes/App';
 
@@ -12,7 +14,9 @@ const initialState = {
   // 'language': localStorage.getItem('language') || 'en',
   'pokemons': [],
   'myPokemons': [],
+  'initialPokemon': INITIAL_POKEMON_NUMBER,
 };
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, initialState, composeEnhancers());
 const $AppContainer = document.getElementById('app');
