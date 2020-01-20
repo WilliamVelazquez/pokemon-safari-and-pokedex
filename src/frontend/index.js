0bf3fdf05bf40e8012c657e91ca046998ca76bf3
William Velazquez <william.velazquez.a@gmail.com>
2020-01-20 13:17:16 -0600

    Setting Browser History and Server Routes

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, compose } from 'redux';
+import { Router } from 'react-router';
+import { createBrowserHistory } from 'history';
 import 'babel-polyfill';
 import './localization/i18n';
 
@@ -20,11 +22,16 @@ const initialState = {
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, initialState, composeEnhancers());
+
+const history = createBrowserHistory();
+
 const $AppContainer = document.getElementById('app');
 
 render(
   <Provider store={store}>
-    <App />
+    <Router history={history}>
+      <App />
+    </Router>
   </Provider>,
   $AppContainer,
 );
